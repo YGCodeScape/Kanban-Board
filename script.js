9d18798353caeb80dfe06e38de0025a8a6659815
yashG <yrg.creation.ai@gmail.com>
2025-12-05 22:17:29 +0530

    new task div

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const progress = document.querySelector("#Progress");
 const done = document.querySelector("#done"); 
 const tasks = document.querySelectorAll('.task');
 const modal = document.querySelector(".modal");
-const toggleModal = document.querySelector("#toggle-modal")
+const modalBg = document.querySelector(".bg")
+const toggleModalBtn = document.querySelector("#toggle-modal")
+const addBtn = document.querySelector("#add-new-task");
+
 
 let dragElement = null;
 
@@ -38,6 +41,30 @@ addDragOverEvent(todo);
 addDragOverEvent(progress);
 addDragOverEvent(done);
 
-toggleModal.addEventListener("click", () => {
-    modal.classList.add("modal-active")
+toggleModalBtn.addEventListener("click", () => {
+    modal.classList.toggle("modal-active")
+})
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("modal-active")
+})
+
+addBtn.addEventListener("click", () => {
+   const taskTitle = document.querySelector("#task-title").value
+   const taskTextarea = document.querySelector("#task-area").value
+   
+   const div = document.createElement("div")
+   div.classList.add("task")
+   div.setAttribute("draggable", "true")
+   
+   div.innerHTML = `
+               <h2 >${taskTitle}</h2>
+               <p>${taskTextarea}</p>
+               <button>Delete</button>  
+            `
+    div.addEventListener("drag", (e) => {
+        dragElement = div;
+    })
+
+    todo.appendChild(div)
+    modal.classList.remove("modal-active")
 })
